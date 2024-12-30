240680ba92e80c22a2e27cb5731f11500f2678ae
Rishi-Kompelli <1108647@apps.nsd.org>
2024-12-30 22:59:32 +0000

    finished

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 abstract class Shape{
     abstract double area();
+    abstract String name();
     void display(){
-        System.out.println("Calculating area...");
+        System.out.println("Calculating area of "+name()+"...");
     }
 }
 
@@ -14,6 +15,44 @@ class Circle extends Shape{
     public double area() {
         return Math.PI*radius*radius;
     }
+    @Override
+    public String name(){
+        return "Circle";
+    }
+}
+
+class Rectangle extends Shape{
+    double length;
+    double width;
+    public Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+    @Override
+    public double area() {
+        return length*width;
+    }
+    @Override
+    public String name(){
+        return "Rectangle";
+    }
+}
+
+class Triangle extends Shape{
+    double base;
+    double height;
+    public Triangle(double base, double height){
+        this.base = base;
+        this.height = height;
+    }
+    @Override
+    public double area() {
+        return 0.5*base*height;
+    }
+    @Override
+    public String name(){
+        return "Triangle";
+    }
 }
 
 public class Main {
@@ -21,5 +60,11 @@ public class Main {
         Shape circle = new Circle(5);
         circle.display();
         System.out.println("Circle Area: "+circle.area());
+        Shape rectangle = new Rectangle(5,5);
+        rectangle.display();
+        System.out.println("Rectangle Area: "+rectangle.area());
+        Shape triangle = new Triangle(10,5);
+        triangle.display();
+        System.out.println("Triangle Area: "+triangle.area());
     }
 }
